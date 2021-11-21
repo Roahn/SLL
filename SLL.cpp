923a3ba64ec9d9fa98e489f26fb7eb742a9a4e93
Rohan <rohan.magar19@vit.edu>
2021-11-21 22:30:05 +0530

    Added Menu Driven functionality and also successfully completed the the insert at position

--- SLL.cpp
+++ SLL.cpp
@@ -107,11 +107,37 @@ int Count(PNODE Head)
 void DeleteAtPos(PPNODE Head , int pos)
 {
 
+    if((pos<1)|| (pos>Count(*Head)+1))
+    {
+        return;
+    }
+    if(*Head == NULL)
+    {
+        DeleteFirst(Head);
+    }
+    else if (pos == Count(*Head)+1)
+    {
+        DeleteLast(Head);
+    }
+    else
+    {
+       
+        PNODE temp = *Head;
+       
+
+        for(int i = 1 ; i<pos;i++)
+        {
+            
+            
+        }
+    }
+
 }
 void InsertAtPos(PPNODE Head , int no ,int pos)
 {
     if((pos<1)|| (pos>Count(*Head)+1))
     {
+        cout<<"Please Enter correct position\n";
         return;
     }
     if(*Head == NULL)
@@ -129,11 +155,12 @@ void InsertAtPos(PPNODE Head , int no ,int pos)
         newn->next =NULL;
         newn ->data = no;
 
-        for(int i = 1 ; i<pos;i++)
+        for(int i = 1 ; i<pos-1;i++)
         {
-            newn->next = temp->next;
-            temp->next = newn;
+           temp = temp->next;
         }
+         newn->next = temp->next;
+            temp->next = newn;
     }
 }
 int main()
@@ -142,26 +169,68 @@ int main()
 
     PNODE first = NULL;
     int ret =0;
+    int choice =1;
+    int no= 0;
+    int pos = 0;
+    bool flag= true;
   
-    Display(first);
-    InsertFirst(&first,10);
-    InsertFirst(&first,10);
-    InsertFirst(&first,10);
-    InsertFirst(&first,10);
-    InsertFirst(&first,10);
-    
-    Display(first);
-    InsertAtPos(&first,20,2);
-    InsertAtPos(&first,30,2);
-    InsertAtPos(&first,50,2);
-    InsertAtPos(&first,40,2);
-    InsertAtPos(&first,560,2);
-    InsertAtPos(&first,820,2);
-
-    
-    Display(first);
-    ret =Count(first);
-    cout<<"Number of elements are "<<ret;
+    while(flag)
+    {
+        cout<<"Enter choice\n";
+       cout<<"Enter 1 for Insert At First position\n";
+       cout<<"Enter 2 for Insert At Last position\n";
+       cout<<"Enter 3 for Insert at desired position\n";
+       cout<<"Enter 4 fot Delete at first position\n";
+       cout<<"Enter 5 for Delete at last position\n";
+       cout<<"Enter 6 for Delete at desired position\n";
+       cout<<"Enter  7 to Display\n";
+        cout<<"Enter choice\n";
+        cin>>choice;
+       switch (choice)
+       {
+       case 1:
+            cout<<"Enter number\n";
+            cin>>no;
+           InsertFirst(&first , no);
+           break;
+       case 2:
+            cout<<"Enter number\n";
+            cin>>no;
+           InsertLast(&first , no);
+           break;
+       case 3:
+            cout<<"Enter number\n";
+            cin>>no;
+            cout<<"Enter Position\n";
+            cin>>pos;
+            InsertAtPos(&first , no,pos);
+           break;
+       case 4:
+           DeleteFirst(&first);
+           break;
+       case 5:
+           DeleteLast(&first);
+           break;
+       case 6:
+            cout<<"Enter Position\n";
+            cin>>pos;
+           DeleteAtPos(&first,pos);
+           break;
+        case 7:
+            Display(first);
+           
+           break;
+       case 0:
+            flag =false;
+           break;
+       
+       default:
+            cout<<"Please Enter correct option\n";
+           break;
+       }
+    }
+    //ret =Count(first);
+    //cout<<"Number of elements are "<<ret;
 
 
 
